c9133d76ec3d88a90aeff4239f14d957cda3d52e
John Woolbright <jwoolbright23@gmail.com>
2023-06-28 13:01:03 -0500

    commented out first hello handler

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -7,18 +7,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello(){
-        return "Hello, Spring!";
-    }
 
+//    //handle requests at path http://localhost:8080/hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
+
+    //handle requests at path http://localhost:8080/goodbye
     @GetMapping("goodbye")
     @ResponseBody
     public String goodbye(){
         return "Goodbye, Spring!";
     }
 
+    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
     @GetMapping("hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name){
