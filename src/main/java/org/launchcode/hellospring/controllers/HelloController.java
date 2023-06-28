32b03cea014909986d2bc4e22bf0641255fe45a5
John Woolbright <jwoolbright23@gmail.com>
2023-06-28 13:18:00 -0500

    class-annotations branch code

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
     //handle request at path http://localhost:8080/hello
 
@@ -13,33 +15,30 @@ public class HelloController {
 //        return "Hello, Spring!";
 //    }
 
-    //handle request at path //http://localhost:8080/goodbye
+    //handle request at path //http://localhost:8080/hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
     public String goodbye(){
         return "Goodbye, Spring!";
     }
 
     //handle requests of the form http://localhost:8080/hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
     //handle requests of the form http://localhost:8080/hello/LaunchCode
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
+    //handles requests for http://localhost:8080/hello/form
     @GetMapping("form")
-    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action = 'hello' method = 'post'>" + // submit a request to /hello
+                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
                 "<input type = 'text' name = 'name' >" +
                 "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
